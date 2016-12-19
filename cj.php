7f565d85479da75ce899fe5a088bec08703a4fe5
cfwinjp12 <cfwinjp12@gmail.com>
2016-12-19 23:15:16 +0900

    first commit

--- cj.php
+++ cj.php
@@ -1,17 +1,7 @@
 <?php
 define('ROOT_PATH', realpath(dirname(__FILE__)));
-include_once ROOT_PATH . '/goutte.phar';
 
 
-
-
-use Goutte\Client;
-use Symfony\Component\DomCrawler\Crawler;
-
-if (!isset($_POST["access_type"])) {
-	die ;
-}
-$access_type 	= $_POST["access_type"];
 $pass 			= $_POST["pass"];
 $url 			= $_POST["url"];
 
@@ -25,23 +15,26 @@ if($pass!=="001100"){
 	die;
 }
 
-$client = new Client();
-$client -> getClient() -> setDefaultOption('config/curl/' . CURLOPT_SSL_VERIFYPEER, false);
-
-//////////Cookie 安装///////////////////////////
-$header_array = array('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36', "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0", );
-$header_str = $header_array[mt_rand(0, (count($header_array) - 1))];
-$client -> setHeader('User-Agent', $header_str);
-/////////////////////////////////////////////////
-
-$crawler=null;
-
-if ("get" === $access_type) {
-	$crawler = $client -> request('GET', $url);
-} elseif ("post" === $access_type) {
-	$crawler = $client->request('POST', $url, $form_data);
+$curl = curl_init();
+$options = array(
+	CURLOPT_URL => $url,
+	CURLOPT_HEADER => false,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_AUTOREFERER => true,
+	CURLOPT_BINARYTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_COOKIESESSION => true,
+	CURLOPT_TIMEOUT => 300,
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_SSL_VERIFYPEER=>false
+);
+if(isset($form_data) && count($form_data)>0){
+	$options[CURLOPT_POSTFIELDS]=$postdata;
+	$options[CURLOPT_POST]=true;
 }
-
-$html = $crawler -> html();
-echo $html;
+curl_setopt_array($curl, $options);
+$content = curl_exec($curl);
+curl_close($curl);
+unset($curl, $options);
+echo $content;
 die;
